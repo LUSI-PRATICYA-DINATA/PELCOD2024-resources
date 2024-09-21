8b85fe0851fcb35946a0342812d1cb9acd94561b
LUSI-PRATICYA-DINATA <praticya054@gmail.com>
2024-09-21 15:17:28 +0700

    bismillah jadi progremer handal

---
+++ main.py
@@ -0,0 +1,8 @@
+nilai = int(input("Masukan nilai: "))
+
+if (nilai > 90 and nilai <= 100):
+    print("Selamat anda lulu, dengan nilai yang memuasakan")
+elif (nilai > 80):
+    print("Selamat anda lulus")
+else:
+    print("Anda masih belum lulus tes, semangat dan coba lagi")
